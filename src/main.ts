874405012f6d46b24da89f33f886fc8b4e6d9b17
Sourav Dey <sourav_d@techjoomla.com>
2019-05-13 13:02:19 +0530

    Issue #SB-6391 feat: Adding lodash in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,15 @@ import { bootstrap, framework } from './index';
 import  { app, BrowserWindow } from 'electron';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as _ from 'lodash';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win: any;
-  let envs = JSON.parse(fs.readFileSync(path.join(__dirname, 'env.json'), { encoding: 'utf-8' }))
-  for (let [key, value] of Object.keys(envs)) {
-    process.env[key] = value;
-  }
+let envs = JSON.parse(fs.readFileSync(path.join(__dirname, 'env.json'), { encoding: 'utf-8' }));
+_.forEach(envs, function (value, key) {
+  process.env[key] = value;
+});
 
 function createWindow() {
   //bootstrap container
